976e35600b51cd6a81886a1a9659104d33ccadf8
Joshua Ferreira <kointaker@Joshuas-MacBook-Air-120.local>
2025-12-17 11:28:57 -0500

    Comment and code cleanup

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def sigmoid(x):
 
 class Neuron:
     def __init__(self, weights, bias):
-        self.weights = weights
         self.bias = bias
 
     def feedforward(self, inputs):
@@ -14,20 +13,25 @@ class Neuron:
         # Calculates this by finding sum of element-wise products
         # [1, 2, 3]
         # [4, 5, 6]
+        self.weights = weights
         # This would be 1*4, 2*5, and 3*6 respectively
         total = np.dot(self.weights, inputs) + self.bias
         return sigmoid(total)
-    
+# weights and bias set outside of class
+# to allow neuron to function with various
+
 weights = np.array([0, 1]) # w1 = 0, w2 = 1
+# inputs/neural networks
 bias = 4                   # b = 4
 n = Neuron(weights, bias) # our neuron now with desired weights and bias
-
 x = np.array([2, 3])       # x1 = 2, x2 = 3
 print(n.feedforward(x))    # 0.9990889488055994
 
 
+
 class OurNeuralNetwork:
     '''
+
     A neural network with:
         - 2 inputs
         - a hidden layer with 2 neurons (h1, h2)
@@ -38,23 +42,30 @@ class OurNeuralNetwork:
     '''
 
     def __init__(self):
+        # weights and bias set for this neural network
         weights = np.array([0, 1])
         bias = 0
 
-        # The Neuron class here is above
+        # Active neurons in this network
         self.h1 = Neuron(weights, bias)
         self.h2 = Neuron(weights, bias)
         self.o1 = Neuron(weights, bias)
 
     def feedforward(self, x):
+        # Both h1 neurons get two inputs
         out_h1 = self.h1.feedforward(x)
         out_h2 = self.h2.feedforward(x)
 
-        # The inputs for o1 are the outputs from h1 and h2 (two previous neurons)
+        # o1 neuron gets two neuron outputs as inputs
         out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
 
         return out_o1
-    
+
+# Creating the neuralnetwork
 network = OurNeuralNetwork()
 x = np.array([2, 3])
+
+# Essentially, h1 and h2 are getting the x as their inputs, then 
+# o1 gets this outputs as it's input, and the output
+# of the network is the output of o1
 print(network.feedforward(x)) # 0.7216325609518421
